461a6d41e980c8f8f1c4ebca261107a33ba5d985
Predrag Jovicic <predragjovicic032@gmail.com>
2024-05-26 21:05:34 +0200

    Added fraud microservice & refactor customer microservice & added comunication between microservices with a RestTemplate

--- customer/src/main/java/com/microservices/customer/CustomerService.java
+++ customer/src/main/java/com/microservices/customer/CustomerService.java
@@ -1,9 +1,16 @@
 package com.microservices.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -11,6 +18,19 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
-        customerRepository.save(customer);
+        // todo: check if email valid
+        // todo: check if email not taken
+        customerRepository.saveAndFlush(customer);
+        // todo: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse != null && fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("Fraudster!");
+        }
+        // todo: send notification
     }
 }
