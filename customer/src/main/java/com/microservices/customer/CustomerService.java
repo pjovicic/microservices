45db2bd1ba9f1476fea7b76b50d32aaec194678f
Predrag Jovicic <predragjovicic032@gmail.com>
2024-05-27 21:38:46 +0200

    Refactoring microservice communication to use OpenFeign instead of RestTemplate

--- customer/src/main/java/com/microservices/customer/CustomerService.java
+++ customer/src/main/java/com/microservices/customer/CustomerService.java
@@ -1,5 +1,7 @@
 package com.microservices.customer;
 
+import com.microservices.clients.fraud.FraudCheckResponse;
+import com.microservices.clients.fraud.FraudClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +14,8 @@ public class CustomerService {
 
     private final RestTemplate restTemplate;
 
+    private final FraudClient fraudClient;
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -22,11 +26,8 @@ public class CustomerService {
         // todo: check if email not taken
         customerRepository.saveAndFlush(customer);
         // todo: check if fraudster
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
 
         if (fraudCheckResponse != null && fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("Fraudster!");
